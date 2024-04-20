676fec33c779c4684ece0dd453a686b0a3b95a1e
Dominique Oyco Work <dominiqueoycowork@Dominiques-Mac-mini.local>
2024-04-19 18:23:21 -0700

    Updated page objects and test files

--- page_objects/login_page.py
+++ page_objects/login_page.py
@@ -3,24 +3,26 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
+from page_objects.base_page import BasePage
 
-class LoginPage:
+
+class LoginPage(BasePage): # Inherit from Base Page class in base_page.py
     __url = "https://practicetestautomation.com/practice-test-login/"
     __username_field = (By.ID, "username")
     __password_field = (By.NAME, "password")
     __submit_button = (By.XPATH, "//button[@class='btn']")
+    __error_message = (By.ID, "error")
 
     def __init__(self, driver: WebDriver):
-        self._driver = driver
+        super().__init__(driver) # supply driver variable to base_page.py
 
     def open(self):
-        self._driver.get(self.__url)
+        super()._open_url(self.__url)
 
     def execute_login(self, username: str, password: str):
-        wait = WebDriverWait(self._driver, 10)
-        wait.until(ec.visibility_of_element_located(self.__username_field))
-        self._driver.find_element(self.__username_field).send_keys(username)
-        wait.until(ec.visibility_of_element_located(self.__password_field))
-        self._driver.find_element(self.__password_field).send_keys(password)
-        wait.until(ec.visibility_of_element_located(self.__submit_button))
-        self._driver.find_element(self.__submit_button).click()
+        super()._type(self.__username_field, username)
+        super()._type(self.__password_field, password)
+        super()._click(self.__submit_button)
+
+    def get_error_message(self) -> str:
+        return super()._get_text(self.__error_message, time=3)
